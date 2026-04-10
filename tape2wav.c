a1990fc5ddeaf00a4712fb7c949673eaeca56409
Fredrick Meunier <fredm@spamcop.net>
2026-04-10 16:10:24 +1000

    Allow tape2wav to write WAV files without libaudiofile
    (thanks, Andre Leiradella) (patch #360).

--- tape2wav.c
+++ tape2wav.c
@@ -27,20 +27,32 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <getopt.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#ifdef WIN32
+#include <io.h>
+#else
 #include <unistd.h>
+#endif
 
 #include "libspectrum.h"
 
-#include <audiofile.h>
-
+#include "compat.h"
 #include "utils.h"
 
 #define PROGRAM_NAME "tape2wav"
+#define WAV_FORMAT_PCM 1
+#define WAV_NUM_CHANNELS 1
+#define WAV_BITS_PER_SAMPLE 8
+#define WAV_FMT_CHUNK_SIZE 16
+#define WAV_HEADER_SIZE 44
 
 static void show_help( void );
 static void show_version( void );
+static int write_all( int fd, const void *buffer, size_t length,
+                      const char *filename );
 static int read_tape( char *filename, libspectrum_tape **tape );
 static int write_tape( char *filename, libspectrum_tape *tape );
 
@@ -168,38 +180,63 @@ read_tape( char *filename, libspectrum_tape **tape )
 }
 
 static int
+write_all( int fd, const void *buffer, size_t length, const char *filename )
+{
+  const libspectrum_byte *ptr = buffer;
+
+  while( length ) {
+    ssize_t written = write( fd, ptr, length );
+
+    if( written < 0 ) {
+      if( errno == EINTR ) continue;
+
+      fprintf( stderr, "%s: error writing to '%s'\n", progname, filename );
+      return 1;
+    }
+
+    if( !written ) {
+      fprintf( stderr, "%s: error writing to '%s'\n", progname, filename );
+      return 1;
+    }
+
+    ptr += written;
+    length -= written;
+  }
+
+  return 0;
+}
+
+static int
 write_tape( char *filename, libspectrum_tape *tape )
 {
-  libspectrum_byte *buffer, *ptr; size_t length;
+  libspectrum_buffer *header_buffer;
+  libspectrum_buffer *sample_buffer;
+  libspectrum_byte *header_data;
+  libspectrum_byte *sample_data;
   size_t tape_length = 0;
   libspectrum_error error;
   short level = 0; /* The last level output to this block */
   libspectrum_dword pulse_tstates = 0;
   libspectrum_dword balance_tstates = 0;
-  int flags = 0;
-  int framesWritten;
-  AFfilehandle file;
-
-  AFfilesetup setup = afNewFileSetup();
+  int close_fd = 0, fd = -1, flags = 0, res = 1;
+  uint32_t subchunk2size;
+  uint32_t byte_rate;
+  uint16_t block_align;
 
   unsigned int scale = 3500000/sample_rate;
 
-  length = 8192;
-  buffer = malloc( length );
-
-  if( !buffer ) {
-    fprintf( stderr, "%s: unable to allocate memory for conversion buffer\n",
-             progname );
-    return 1;
-  }
+  header_buffer = libspectrum_buffer_alloc();
+  sample_buffer = libspectrum_buffer_alloc();
+  header_data = NULL;
+  sample_data = NULL;
 
   while( !(flags & LIBSPECTRUM_TAPE_FLAGS_TAPE) ) {
     libspectrum_dword pulse_length = 0;
-    size_t i;
 
     error = libspectrum_tape_get_next_edge( &pulse_tstates, &flags, tape );
     if( error != LIBSPECTRUM_ERROR_NONE ) {
-      free( buffer );
+      libspectrum_buffer_free( header_buffer );
+      libspectrum_buffer_free( sample_buffer );
       return 1;
     }
 
@@ -236,74 +273,73 @@ write_tape( char *filename, libspectrum_tape *tape )
       balance_tstates = 0;
     }
 
-    while( tape_length + pulse_length > length ) {
-      length *= 2;
-      ptr = buffer;
-      buffer = realloc( buffer, length );
-      if( !buffer ) {
-        free( ptr );
-        fprintf( stderr, "%s: unable to allocate memory for conversion buffer\n",
-                 progname );
-        return 1;
-      }
-    }
-
-    for( i = 0; i < pulse_length; i++ ) {
-      buffer[ tape_length++ ] = level ? 0xff : 0x00;
-    }
+    libspectrum_buffer_set( sample_buffer, level ? 0xff : 0x00,
+                            pulse_length );
   }
 
-  afInitFileFormat( setup, AF_FILE_WAVE );
-  afInitChannels( setup, AF_DEFAULT_TRACK, 1 );
-  afInitSampleFormat( setup, AF_DEFAULT_TRACK, AF_SAMPFMT_UNSIGNED, 8 );
-  afInitRate( setup, AF_DEFAULT_TRACK, sample_rate );
+  tape_length = libspectrum_buffer_get_data_size( sample_buffer );
+  sample_data = libspectrum_buffer_get_data( sample_buffer );
+  subchunk2size = tape_length;
+  byte_rate = sample_rate * WAV_NUM_CHANNELS * WAV_BITS_PER_SAMPLE / 8;
+  block_align = WAV_NUM_CHANNELS * WAV_BITS_PER_SAMPLE / 8;
+
+  libspectrum_buffer_write( header_buffer, "RIFF", 4 );
+  libspectrum_buffer_write_dword( header_buffer,
+                                  WAV_HEADER_SIZE - 8 + subchunk2size );
+  libspectrum_buffer_write( header_buffer, "WAVE", 4 );
+  libspectrum_buffer_write( header_buffer, "fmt ", 4 );
+  libspectrum_buffer_write_dword( header_buffer, WAV_FMT_CHUNK_SIZE );
+  libspectrum_buffer_write_word( header_buffer, WAV_FORMAT_PCM );
+  libspectrum_buffer_write_word( header_buffer, WAV_NUM_CHANNELS );
+  libspectrum_buffer_write_dword( header_buffer, sample_rate );
+  libspectrum_buffer_write_dword( header_buffer, byte_rate );
+  libspectrum_buffer_write_word( header_buffer, block_align );
+  libspectrum_buffer_write_word( header_buffer, WAV_BITS_PER_SAMPLE );
+  libspectrum_buffer_write( header_buffer, "data", 4 );
+  libspectrum_buffer_write_dword( header_buffer, subchunk2size );
+
+  header_data = libspectrum_buffer_get_data( header_buffer );
 
   if( strncmp( filename, "-", 1 ) == 0 ) {
-    int fd = fileno( stdout );
+    fd = fileno( stdout );
     if( isatty( fd ) ) {
       fprintf( stderr, "%s: won't output binary data to a terminal\n",
                progname );
-      free( buffer );
-      afFreeFileSetup( setup );
-      return 1;
+      goto out;
     }
+  } else {
+    fd = open( filename, O_WRONLY | O_CREAT | O_TRUNC | O_BINARY, 0666 );
+    if( fd == -1 ) {
+      fprintf( stderr, "%s: unable to open file '%s' for writing\n", progname,
+               filename );
+      goto out;
+    }
+    close_fd = 1;
+  }
 
 #ifdef WIN32
-    setmode( fd, O_BINARY );
-#endif				/* #ifdef WIN32 */
+  setmode( fd, O_BINARY );
+#endif                          /* #ifdef WIN32 */
 
-    file = afOpenFD( fd, "w", setup );
-  } else {
-    file = afOpenFile( filename, "w", setup );
-  }
-  if( file == AF_NULL_FILEHANDLE ) {
-    fprintf( stderr, "%s: unable to open file '%s' for writing\n", progname,
-             filename );
-    free( buffer );
-    afFreeFileSetup( setup );
-    return 1;
+  if( write_all( fd, header_data,
+                 libspectrum_buffer_get_data_size( header_buffer ), filename ) ) {
+    goto out;
   }
 
-  framesWritten = afWriteFrames( file, AF_DEFAULT_TRACK, buffer, tape_length );
-  if(framesWritten != tape_length ) {
-    fprintf( stderr,
-             "%s: number of frames written does not match number of frames"
-             " requested\n",
-             progname );
-    free( buffer );
-    afFreeFileSetup( setup );
-    return 1;
+  if( write_all( fd, sample_data, subchunk2size, filename ) ) {
+    goto out;
   }
 
-  if( afCloseFile( file ) ) {
-    fprintf( stderr, "%s: error closing wav file\n", progname );
-    free( buffer );
-    afFreeFileSetup( setup );
-    return 1;
+  res = 0;
+
+out:
+
+  if( close_fd && close( fd ) != 0 ) {
+    fprintf( stderr, "%s: error closing '%s'\n", progname, filename );
   }
 
-  free( buffer );
-  afFreeFileSetup( setup );
+  libspectrum_buffer_free( header_buffer );
+  libspectrum_buffer_free( sample_buffer );
 
-  return 0;
+  return res;
 }
